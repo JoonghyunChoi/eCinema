3f6eb29863de72cd3656906290588dbd8fc8d069
최중현 <wnd3735@gmail.com>
2021-09-20 21:53:23 +0900

    Change directory(recommit)

--- src/main/java/ecinema/data/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package ecinema.data;
-
-import ecinema.domain.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
